f8539552a712df574c741213ebac1af882efc3e5
ash-eff <ash.s.fischer@gmail.com>
2023-09-13 20:51:14 -0500

    Complete form feature

--- email_tool/views.py
+++ email_tool/views.py
@@ -9,7 +9,8 @@ from django import forms
 class ProjectSelectionView(View):
     def get(self, request):
         form = ProjectSelectionForm()
-        return render(request, 'home.html', {'form': form})
+        selected_project = None  # Set selected_project to None for this view
+        return render(request, 'home.html', {'form': form, 'selected_project': selected_project})
     
     def post(self, request):
         form = ProjectSelectionForm(request.POST)
@@ -28,24 +29,18 @@ class ProjectLandingPageView(View):
         return render(request, 'project-landing-page.html', {'selected_project': selected_project, 'email_templates': email_templates, 'selected_project_name': selected_project_name})
     
     def post(self, request, name):
-        selected_project_name = name.upper()
-        selected_project = get_object_or_404(Project, name=name)
         template_subject = request.POST.get('template_subject', None)
-        selected_template = selected_project.email_templates.get(subject=template_subject)
-        print(selected_template.template_text)
-        return render(request, 'email-template.html', {'selected_project': selected_project, 'selected_template': selected_template, 'selected_project_name': selected_project_name})
-    
+        return HttpResponseRedirect(reverse('email-template', args=[name, template_subject]))
+     
 class CreateEmailView(View):
-    
     def get(self, request, name, email_subject):
+        selected_project_name = name.upper()
         selected_project = get_object_or_404(Project, name=name)
         template = get_object_or_404(EmailTemplate, subject=email_subject)
         template_forms = template.custom_form_template
-
         form_fields = {}
         for field in template_forms.fields.all():
             field_name = field.label
-            print(field_name)
             field_type = field.field_type
             field_choices = field.choices.split(',') if field.choices else []
             field_required = field.required
@@ -53,35 +48,35 @@ class CreateEmailView(View):
             if field_type == 'CharField':
                 form_fields[field_name] = forms.CharField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.TextInput(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'EmailField':
                 form_fields[field_name] = forms.EmailField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.EmailInput(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'ChoiceField':
                 form_fields[field_name] = forms.ChoiceField(
                     choices=[(choice.strip(), choice.strip()) for choice in field_choices],
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.Select(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'IntegerField':
                 form_fields[field_name] = forms.IntegerField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.TextInput(attrs={'class': 'form-control'}),
                 )
 
         CustomEmailForm = type('CustomEmailForm', (forms.Form,), form_fields)
         form = CustomEmailForm
-        #print('form: ' + str(form_fields))  
-        return render(request, 'email-template.html', {'form': form})
+        return render(request, 'email-template.html', {'form': form, 'selected_project': selected_project, 'selected_project_name': selected_project_name})
     
     def post(self, request, name, email_subject):
+        selected_project_name = name.upper()
         selected_project = get_object_or_404(Project, name=name)
         template = get_object_or_404(EmailTemplate, subject=email_subject)
         template_forms = template.custom_form_template
@@ -96,26 +91,26 @@ class CreateEmailView(View):
             if field_type == 'CharField':
                 form_fields[field_name] = forms.CharField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.TextInput(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'EmailField':
                 form_fields[field_name] = forms.EmailField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.EmailInput(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'ChoiceField':
                 form_fields[field_name] = forms.ChoiceField(
                     choices=[(choice.strip(), choice.strip()) for choice in field_choices],
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.Select(attrs={'class': 'form-control'}),
                 )
             elif field_type == 'IntegerField':
                 form_fields[field_name] = forms.IntegerField(
                     required=field_required,
-                    label=field_name.title(),
+                    label=field_name,
                     widget=forms.TextInput(attrs={'class': 'form-control'}),
                 )
 
@@ -127,6 +122,6 @@ class CreateEmailView(View):
             template_text = template.template_text
             formatted_text = template_text.format(**form.cleaned_data)
             
-            return render(request, 'email-template.html', {'form': form, 'formatted_text':  formatted_text,})
+            return render(request, 'email-template.html', {'form': form, 'formatted_text':  formatted_text, 'selected_project': selected_project, 'selected_project_name': selected_project_name})
         else:
             return render(request, 'email-template.html', {'form': form})
